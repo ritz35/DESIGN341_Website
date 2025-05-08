8ba6f1255427e6e63b34f658e1393af4238c5f25
rs379 <rs379@student.waikato.ac.nz>
2025-05-08 19:42:14 +1200

    COMMIT 3: added selected and hover effects for navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import '../css/navbar.css'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 function Navbar(){
   return(
       <div className='div_navbar'>
         <nav className='navbar'>
           <ul>
-            <li><Link className="link_pages" to="/">HOME</Link></li>
-            <li><Link className="link_pages" to="cohort">STUDENT COHORT</Link></li> {/*this needs a search function */}
-            <li><Link className="link_pages" to="portfolio">PORTFOLIO</Link></li> {/*this needs a video */}
-            <li><Link className="link_pages" to="contacts">CONTACT</Link> </li>
+            <li><NavLink className="link_pages" to="/">HOME</NavLink></li>
+            <li><NavLink className="link_pages" to="cohort">STUDENT COHORT</NavLink></li> {/*this needs a search function */}
+            <li><NavLink className="link_pages" to="portfolio">PORTFOLIO</NavLink></li> {/*this needs a video */}
+            <li><NavLink className="link_pages" to="contacts">CONTACT</NavLink> </li>
           </ul>
         </nav>
       </div>
