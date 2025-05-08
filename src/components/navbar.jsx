33511e08d56d75451c60b8952f1eb7d6b8c1919e
rs379 <rs379@student.waikato.ac.nz>
2025-05-08 19:28:15 +1200

    COMMIT 2: Homepage done, commented out the videos because they would crash the site, will fix later.
    Git ignoring the videos from the public folder
    
    Fixed a lot of div placement issues, looks a lot nicer now

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,10 +7,10 @@ function Navbar(){
       <div className='div_navbar'>
         <nav className='navbar'>
           <ul>
-            <li><Link to="/">HOME</Link></li>
-            <li><Link to="cohort">STUDENT COHORT</Link></li> {/*this needs a search function */}
-            <li><Link to="portfolio">PORTFOLIO</Link></li> {/*this needs a video */}
-            <li><Link to="contacts">CONTACT</Link> </li>
+            <li><Link className="link_pages" to="/">HOME</Link></li>
+            <li><Link className="link_pages" to="cohort">STUDENT COHORT</Link></li> {/*this needs a search function */}
+            <li><Link className="link_pages" to="portfolio">PORTFOLIO</Link></li> {/*this needs a video */}
+            <li><Link className="link_pages" to="contacts">CONTACT</Link> </li>
           </ul>
         </nav>
       </div>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,18 +1,47 @@
 import { useState } from "react";
 import '../css/homepage.css'
+import {Link} from 'react-router-dom'
 
 function Homepage(){
     return(
         <div className="homepage_wrapper">
+            <Link className="link_pages" to="cohort">
             <div className="div_cohort">
-                <h1>STUDENT COHORT</h1>
+                <video autoPlay muted loop playsInline className="background_video">
+                    {/*<source src="video/test_video.mp4" type="video/mp4"/>*/}
+                    your browser does not support the video tag
+                </video>
+
+                <div className="content_overlay">
+                    <h1>STUDENT COHORT</h1>
+                </div>
             </div>
-            <div className="div_port">
-                <h1>PORTFOLIO</h1>
-            </div>
-            <div className="div_contact">
-                <h1>CONTACT</h1>
-            </div>
+            </Link>
+
+            <Link className="link_pages" to="portfolio">
+                <div className="div_port">
+                <video autoPlay muted loop playsInline className="background_video">
+                    {/*<source src="video/test_video2.mp4" type="video/mp4"/>*/}
+                    your browser does not support the video tag
+                </video>
+
+                    <div className="content_overlay">
+                        <h1>PORTFOLIO</h1>
+                    </div>
+                </div>
+            </Link>
+            <Link className="link_pages" to="contacts">
+                <div className="div_contact">
+                    <video autoPlay muted loop playsInline className="background_video">
+                        {/*<source src="video/test_video3.mp4" type="video/mp4"/>*/}
+                        your browser does not support the video tag
+                    </video>
+
+                    <div className="content_overlay">
+                        <h1>CONTACTS</h1>
+                    </div>
+                </div>
+            </Link>
         </div>
     )
 }
