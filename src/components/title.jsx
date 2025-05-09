1a9959612abf158fcd3d51b425813e861e9dbe73
rs379 <rs379@student.waikato.ac.nz>
2025-05-09 19:48:11 +1200

    COMMIT 7:
    a lot of assets being added on.
    
    worked on responsiveness of it all, made the title not opacitate (due to the svg file itself) changeed the background to a kinda better wireframe image, just need to add heaps of other assets in the background, hopefully animate them too

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import titleLogoWhite from '../assets/title_white.svg'
+import bottombar from '../assets/bottombar_blue.svg'
+import blue_globe from '../assets/blue_globe.svg'
 import '../css/title.css'
 
 function Title(){
     return(
-        <div className='title'>
-            <img src={titleLogoWhite} className='title_name'/>
+        <div className='title_wrapper'>
+            <div className='title'>
+                <div className='title_inner'>
+                    <img src={blue_globe} className='asset_blueglobe'/>
+                    <img src={titleLogoWhite} className='title_name'/>
+                </div>
+            </div>
+            <div className='asset_bottombar'>
+                <img src={bottombar} />
+            </div>
         </div>
     )
 }
