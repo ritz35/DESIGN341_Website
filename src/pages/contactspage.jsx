00ce77064bf6e1c6333c4af3389e44dd5fb6fe3d
rs379 <rs379@student.waikato.ac.nz>
2025-05-09 16:28:08 +1200

    COMMIT6: implemented a quick contacts page, package.json I accidently redownloaded a thing.

--- src/pages/contactspage.jsx
+++ src/pages/contactspage.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
+import '../css/contactpage.css';
 
-function Contacts(){
-
+function ContactPage(){
+  return(
+    <div className="contact_wrapper">
+      <div className="contact_card">
+        <h1>Contact Us</h1>
+        <form className="contact_form">
+          <input type="text" placeholder="Your Name" required />
+          <input type="email" placeholder="Your Email" required />
+          <textarea placeholder="Your Message" rows="6" required />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contacts
+export default ContactPage;
