c7c95b0ee25c6f8b38c611519fdc2a159449083d
rs379 <rs379@student.waikato.ac.nz>
2025-05-09 21:22:12 +1200

    COMMIT 8.1.2: Fixed the navigation, of the homepage for github pages

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ function Navbar(){
       <div className='div_navbar'>
         <nav className='navbar'>
           <ul>
-            <li><NavLink className="link_pages" to="/">HOME</NavLink></li>
+            <li><NavLink className="link_pages" to="/DESIGN341_Website">HOME</NavLink></li>
             <li><NavLink className="link_pages" to="cohort">STUDENT COHORT</NavLink></li> {/*this needs a search function */}
             <li><NavLink className="link_pages" to="portfolio">PORTFOLIO</NavLink></li> {/*this needs a video */}
             <li><NavLink className="link_pages" to="contacts">CONTACT</NavLink> </li>
