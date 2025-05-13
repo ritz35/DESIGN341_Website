c957c030ccc64a9db06042c25efbbb7956b9f2ce
rs379 <rs379@student.waikato.ac.nz>
2025-05-14 09:44:49 +1200

    COMMIT 12.1: added a cool animation that changes the nav bar when hovered, from png to the actual navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,25 @@
 import { useState } from 'react'
 import '../css/navbar.css'
 import {NavLink} from 'react-router-dom'
+import img_sidebar from '../assets/sidebar.svg'
 
 function Navbar(){
   return(
-      <div className='div_navbar'>
-        <nav className='navbar'>
-          <ul>
-            <li><NavLink className="link_pages" to="/DESIGN341_Website/home">HOME</NavLink></li>
-            <li><NavLink className="link_pages" to="/DESIGN341_Website/cohort">STUDENT COHORT</NavLink></li> {/*this needs a search function */}
-            <li><NavLink className="link_pages" to="/DESIGN341_Website/portfolio">PORTFOLIO</NavLink></li> {/*this needs a video */}
-            <li><NavLink className="link_pages" to="/DESIGN341_Website/contacts">CONTACT</NavLink> </li>
-          </ul>
-        </nav>
+        <div className="navbar_wrapper">
+          <img className='nav_image' src={img_sidebar}></img>
+          <div className='div_navbar'>
+            <nav className='navbar'>
+              <ul>
+                <li><NavLink className="link_pages" to="/DESIGN341_Website/home">HOME</NavLink></li>
+                <li><NavLink className="link_pages" to="/DESIGN341_Website/cohort">STUDENT COHORT</NavLink></li> {/*this needs a search function */}
+                <li><NavLink className="link_pages" to="/DESIGN341_Website/portfolio">PORTFOLIO</NavLink></li> {/*this needs a video */}
+                <li><NavLink className="link_pages" to="/DESIGN341_Website/contacts">CONTACT</NavLink> </li>
+              </ul>
+            </nav>
+          </div>
       </div>
   )
+
 }
 
 export default Navbar
