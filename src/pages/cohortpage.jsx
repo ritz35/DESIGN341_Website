19188c4c23fe075a633b8ad1ca3dd074c42afd0e
rs379 <rs379@student.waikato.ac.nz>
2025-05-17 11:25:03 +1200

    Commit 18: got the pop up to work, need to add a design to have an exit button

--- src/pages/cohortpage.jsx
+++ src/pages/cohortpage.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Searchbar from "../components/seachbar"
 import '../css/cohort.css'
 
 function Cohort(){
     const [students, setStudents] = useState([]);
+    /*This is for a module that opens up whenever a student is hovered*/
+    const [hoveredStudent, setHoveredStudent] = useState(null)
+    /*this hover logic makes sure the pop up stays*/
+    const [hoverTimeout, setHoverTimeout] = useState(null);
+
 
     useEffect(() => {
         fetch('/DESIGN341_Website/data/studentsData.json')
@@ -11,6 +17,17 @@ function Cohort(){
             .then(data => setStudents(data))
             .catch(error => console.error("error in cohort page loading the students data: ", error));
     }, []);
+
+    const handleMouseEnter = (student) => {
+      //clearTimeout(hoverTimeout); // Cancel any pending hide
+      setHoveredStudent(student);
+    };
+
+    const handleMouseLeave = () => {
+      const timeout = setTimeout(() => setHoveredStudent(null), 200);
+      setHoverTimeout(timeout);
+    };
+
 
     return(
         <div className="cohort_wrapper">
@@ -24,15 +41,36 @@ function Cohort(){
 
             <div className="cohort_grid">
                 {students.map((student) =>(
-                    <div className="student_card" key={student.id}>
+                    <div 
+                        className="student_card"
+                        key={student.id}
+                        onMouseEnter={() => handleMouseEnter(student)}
+                        >
                         <img src={student.image} alt={student.name} className="student_image" />
                         <h2>{student.name}</h2>
                         <h3>{student.role}</h3>
                     </div>
                 ))}
             </div>
+
+            {hoveredStudent && (
+                <div className="student_popup"
+                    onMouseEnter={() => handleMouseEnter(student)}
+                    onMouseLeave={handleMouseLeave}>
+                    <div className="popup_content">
+                        <img src={hoveredStudent.image} alt={hoveredStudent.name} className="popup_image"/>
+                        <h1>{hoveredStudent.name}</h1>
+                        <h2>{hoveredStudent.role}</h2>
+                        <p>{hoveredStudent.biography}</p>
+
+                        <Link to={`/DESIGN341_Website/student/${hoveredStudent.id}`} className="view_profile_button">
+                            View Full Profile →
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default Cohort
