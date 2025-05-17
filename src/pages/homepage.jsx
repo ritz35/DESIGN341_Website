e724b795ff36d03f6056e8c5c56d99bb4ea399cb
rs379 <rs379@student.waikato.ac.nz>
2025-05-17 16:51:15 +1200

    COMMIT23.1: homepage information added

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import '../css/homepage.css'
 import {Link} from 'react-router-dom'
 import fb_city from '../assets/city.svg'
@@ -7,45 +7,61 @@ import fb_planet from '../assets/planet.svg'
 
 
 function Homepage(){
+    const [infoData, setInfoData] = useState([]);
+
+    useEffect(() => {
+        fetch('/DESIGN341_Website/data/homepage_information.json')
+            .then(res => res.json())
+            .then(data => setInfoData(data))
+            .catch(err => console.error(err));
+    }, []);
+
+
     return(
         <div className="homepage_wrapper">
-            <Link className="link_pages" to="/DESIGN341_Website/cohort">
-            <div className="div_cohort">
-                <video autoPlay muted loop playsInline poster={fb_city} className="background_video">
-                    {/* <source src="/DESIGN341_Website/video/test_video.mp4" type="video/mp4"/> */}
-                    your browser does not support the video tag
-                </video>
-
-                <div className="content_overlay">
-                    <h1>STUDENT COHORT</h1>
-                </div>
+            <div className="intro_wrapper">
+                {infoData.map(item => (
+                    <h4 key={item.id} className="hp_para">{item.info}</h4>
+                ))}
             </div>
-            </Link>
-
-            <Link className="link_pages" to="/DESIGN341_Website/portfolio">
-                <div className="div_port">
-                <video autoPlay muted loop playsInline poster={fb_planet} className="background_video">
-                    {/* <source src="/DESIGN341_Website/video/test_video2.mp4" type="video/mp4"/> */}
-                    your browser does not support the video tag
-                </video>
-
-                    <div className="content_overlay">
-                        <h1>PORTFOLIO</h1>
-                    </div>
-                </div>
-            </Link>
-            <Link className="link_pages" to="/DESIGN341_Website/contacts">
-                <div className="div_contact">
-                    <video autoPlay muted loop playsInline poster={fb_galaxy} className="background_video">
-                        {/* <source src="/DESIGN341_Website/video/test_video3.mp4" type="video/mp4"/> */}
+            <div>
+                <Link className="link_pages" to="/DESIGN341_Website/cohort">
+                <div className="div_cohort">
+                    <video autoPlay muted loop playsInline poster={fb_city} className="background_video">
+                        {/* <source src="/DESIGN341_Website/video/test_video.mp4" type="video/mp4"/> */}
                         your browser does not support the video tag
                     </video>
 
                     <div className="content_overlay">
-                        <h1>CONTACTS</h1>
+                        <h1>STUDENT COHORT</h1>
                     </div>
                 </div>
-            </Link>
+                </Link>
+                <Link className="link_pages" to="/DESIGN341_Website/portfolio">
+                    <div className="div_port">
+                    <video autoPlay muted loop playsInline poster={fb_planet} className="background_video">
+                        {/* <source src="/DESIGN341_Website/video/test_video2.mp4" type="video/mp4"/> */}
+                        your browser does not support the video tag
+                    </video>
+
+                        <div className="content_overlay">
+                            <h1>PORTFOLIO</h1>
+                        </div>
+                    </div>
+                </Link>
+                <Link className="link_pages" to="/DESIGN341_Website/contacts">
+                    <div className="div_contact">
+                        <video autoPlay muted loop playsInline poster={fb_galaxy} className="background_video">
+                            {/* <source src="/DESIGN341_Website/video/test_video3.mp4" type="video/mp4"/> */}
+                            your browser does not support the video tag
+                        </video>
+
+                        <div className="content_overlay">
+                            <h1>CONTACTS</h1>
+                        </div>
+                    </div>
+                </Link>
+            </div>
         </div>
     )
 }
