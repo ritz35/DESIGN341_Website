257113395cb1ae82dde8d381b9239420fd2c0ab8
rs379 <rs379@student.waikato.ac.nz>
2025-05-17 17:27:08 +1200

    commit 23.3: added more information to the footer, the information added should complete all the information needed to be added.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,21 @@
 import '../css/footer.css';
+import unilogo from '../assets/unilogowhite.svg'
+import unilogoblack from '../assets/unilogoblack.svg'
 
 function Footer() {
     return (
         <footer className="site-footer">
             <div className="footer-content">
-                <p>&copy; {new Date().getFullYear()} DESIGN341. All rights reserved.</p>
+                <p className='l1'>A showcase of projects created by final-year Bachelor of Design students</p>
+                <p className='l2'>&copy; {new Date().getFullYear()} www.designshow.waikato.ac.nz</p>
+                                <div className='footer-image'>
+                    <img src={unilogoblack} />
+                </div>
+                
                 <div className="footer-links">
-                    <a href="/about">About</a>
-                    <a href="/cohort">Cohort</a>
-                    <a href="/portfolio">Portfolio</a>
+                    <a href="/DESIGN341_Website/cohort">Cohort</a>
+                    <a href="/DESIGN341_Website/portfolio">Portfolio</a>
+                    <a href="/DESIGN341_Website/contacts">Contacts</a>
                 </div>
             </div>
         </footer>
