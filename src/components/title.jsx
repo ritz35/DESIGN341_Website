d8431e30bd5c4c8cd1eb8b98df793084d9d0b212
rs379 <rs379@student.waikato.ac.nz>
2025-05-17 17:02:27 +1200

    COMMIT 23.2: Added tagline and spaced it

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -3,6 +3,7 @@ import titleLogoWhite from '../assets/title_white.svg'
 import bottombar from '../assets/bottombar_blue.svg'
 import blue_globe from '../assets/blue_globe.svg'
 import title_hollow from '../assets/hollowed_v2.svg'
+import tagline from '../assets/TAGLINE.svg'
 import '../css/title.css'
 
 function Title(){
@@ -14,6 +15,9 @@ function Title(){
                     <img src={titleLogoWhite} className='asset_title'/>
                     <img src={title_hollow} className='asset_title_hollow'/>
                 </div>
+            </div>
+            <div className='div_tagline'>
+                <img src={tagline} className='asset_tagline'/>
             </div>
             <div className='asset_bottombar'>
                 <img src={bottombar} />
